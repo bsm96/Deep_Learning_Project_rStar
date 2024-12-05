d0f93e9ee63683f99c581bc92aca8a25fd03b456
Bjørn <s224184@dtu.dk>
2024-12-05 21:20:19 +0100

    rename

--- job_STG_disk2.sh
+++ job_STG_disk2.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-#BSUB -q c02516
-#BSUB -J rStar_Project_STG_disk2
+#BSUB -q gpua100
+#BSUB -J rStar_Project_STG_rollout2
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -R "span[hosts=1]"    # Ensures that all cores or tasks requested by the job run on a single host (node)
 #BSUB -R "rusage[mem=16GB]"
 #BSUB -M 16GB
 #BSUB -W 12:00
-#BSUB -o rStar_Output_rStar_Project_STG_disk2_%J.out
-#BSUB -e rStar_Error_rStar_Project_STG_disk2_%J.err
+#BSUB -o rStar_Output_rStar_Project_STG_rollout2_%J.out
+#BSUB -e rStar_Error_rStar_Project_STG_rollout2_%J.err
 
 
 module load python3/3.10.14
@@ -24,4 +24,4 @@ nvidia-smi > gpu_status_${LSB_JOBID}.log
 source /zhome/d6/a/186925/02356_Deep_Learning/Deep_learning_project_2024/02356_Deep_Learning_Project/bin/activate
 cd /zhome/d6/a/186925/Deep_Learning/rStar
 
-bash /zhome/d6/a/186925/Deep_Learning/rStar/scripts/run_STG_generator_disk2.sh --gpu_memory_utilization 0.9
+bash /zhome/d6/a/186925/Deep_Learning/rStar/scripts/run_STG_generator_rollout2.sh --gpu_memory_utilization 0.9
